ed251924141a23c01c93655d2ff0fca1e5ffd35c
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-02-19 23:53:14 +0200

    123

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'normalize.css';
+
 import App from './components/App/App';
 import store, { persistor } from './redux/store';
 import { Provider } from 'react-redux';
