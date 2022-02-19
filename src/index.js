ad3225d2fbfe7819ea7a445b67241d55a3a054e7
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-02-19 23:32:37 +0200

    123

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "normalize.css";
-import App from "./components/App/App";
-import persistor, {store}  from './redux/store';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+import App from './components/App/App';
+import { persistor, store } from './redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-
-
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
+      <PersistGate loading={null} persistor={persistor}>
         <App />
-     </PersistGate> 
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,52 +1,49 @@
-import { configureStore, getDefaultMiddleware  } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { createAction, createReducer, combineReducers } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
-import { persistStore, persistReducer } from 'redux-persist'
+import { persistStore, persistReducer } from 'redux-persist';
 
-const initialState =[
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]
+const initialState = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
 export const add = createAction('myItems/add');
 export const remove = createAction('myItems/remove');
 
-
-
-const myReducerItems = createReducer(initialState,{
-    [add]:(state, action) => [action.payload, ...state],
-    [remove]: (state, action) => {
-        return state.filter(contact => contact.id !== action.payload);
-      },
-})
+const myReducerItems = createReducer(initialState, {
+  [add]: (state, action) => [action.payload, ...state],
+  [remove]: (state, action) => {
+    return state.filter(contact => contact.id !== action.payload);
+  },
+});
 // -----------------------------------------------------------------------------
 const initialStateFilter = '';
 
 export const setFilter = createAction('contacts/setFilter');
 
 const myFilter = createReducer(initialStateFilter, {
-    [setFilter]: (state, action) => {
-      return state = action.payload;
-    },
-  });
+  [setFilter]: (state, action) => {
+    return (state = action.payload);
+  },
+});
 
 // -----------------------------------------------------------------------------
-
 
 const persistConfig = {
   key: 'contactsBook',
   storage,
-  blacklist: ['filter']
-}
+  blacklist: ['filter'],
+};
 
 const rootReducer = combineReducers({
   items: myReducerItems,
-  filter: myFilter
-})
+  filter: myFilter,
+});
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
@@ -55,8 +52,6 @@ export const store = configureStore({
       ignoredActions: ['persist/PERSIST'],
     },
   }),
-})
+});
 
- const persistor = persistStore(store)
-
-export default persistor
+export const persistor = persistStore(store);
